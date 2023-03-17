08a1ee864bfed0d45ae2d9e349b1bb76fa6d29e4
Paritosh Maurya <paritosh.maurya@tadigital.com>
2023-03-17 19:45:08 +0530

    Added code for custom event tracking and css and enabled youtube api

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const shareBtnEle = document.querySelectorAll('.custom-event-trigger');
+const formEle = document.querySelectorAll('form');
+
+formEle.forEach(ele=>{
+   ele.addEventListener('submit', (e)=>{
+      e.preventDefault();
+      console.log(e)
+      gtag('event', 'form_submit', {
+         form_id: e.target.id
+      })
+   })
+})
+
+shareBtnEle.forEach(ele=>{
+   console.log(ele);
+   ele.addEventListener('click', (e)=>{
+      gtag('event', 'share_click', {
+         btn_name: e.target.innerText,
+      }) 
+   })
+})
